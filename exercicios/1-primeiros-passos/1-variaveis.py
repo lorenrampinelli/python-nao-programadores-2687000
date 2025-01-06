d908fd2ed10a80824435866af173c8867194862a
Loren Rampinelli <72108687+lorenrampinelli@users.noreply.github.com>
2025-01-06 16:15:17 +0000

    exercicio variaves done

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,14 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Loren'
+usuario = 'user1'
 
 # Declare variáveis de cada tipo numérico
-
+kilo = 1.5
+idade = 10
+complexo = 2j+1
 
 # Declare uma variável do tipo booleano
+booleano1 = True
+booleano2 = False
