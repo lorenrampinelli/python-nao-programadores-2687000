0ba26acdfe46ca2e5cb8d09a4e8747e3debac2d0
Loren Rampinelli <72108687+lorenrampinelli@users.noreply.github.com>
2025-01-06 21:10:56 +0000

    exercicio lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,22 @@
 # Crie uma lista apenas com elementos numéricos
-
+idades = [1, 3, 4, 6, 7, 11]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+lista_mista = [1, "oi", False, 1.09, 2j, 4, "Loren"]
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
+print(lista_mista)
+print(lista_mista[0])
+print(lista_mista[0:5])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+print(lista_mista[0:-1:2])
 
 # Remova da lista o último item
+lista_mista.pop()
 
 # Insira na lista um novo item
+lista_mista.append("Izzie")
 
 # Remova da lista um item específico
+lista_mista.remove("Izzie")
+print(lista_mista)
