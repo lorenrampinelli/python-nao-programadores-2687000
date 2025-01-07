1e45bb6f7c859a4461c7fc2ca0453f69e36a2ddd
Loren Rampinelli <72108687+lorenrampinelli@users.noreply.github.com>
2025-01-07 15:17:21 +0000

    Soltei linhas

--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -13,4 +13,7 @@ curso = input("Curso estudado: ")
 # 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
 total_dias = int(total_dias)
 total_horas = int(total_horas)
-print(f"{nome}, voce costuma estudar {total_horas} horas por dia, {total_dias} dias por semana. Isso significa que voce estuda {curso}, em media, {total_dias*total_horas} horas por semana")
+print(f"{nome}, voce costuma estudar {total_horas} horas por dia, 
+{total_dias} dias por semana. 
+Isso significa que voce estuda {curso}, 
+em media, {total_dias*total_horas} horas por semana")
