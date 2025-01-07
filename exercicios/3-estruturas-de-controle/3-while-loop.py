a5c8720e1c63aca5890a4f01332097922d2fed01
Loren Rampinelli <72108687+lorenrampinelli@users.noreply.github.com>
2025-01-07 20:03:59 +0000

    exercicio while loop done

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,12 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
-
+primeiro_termo = 1
+ultimo_termo = 6
 
 # Crie um while loop que imprima na tela o nível atual
-
+while primeiro_termo <=ultimo_termo:
+  print (f"Voce esta no termo {primeiro_termo}")
+  primeiro_termo += 1
+else:
+  print ("Parabes, vc se formou!")
 
 # Insira "else" no while loop anterior.
